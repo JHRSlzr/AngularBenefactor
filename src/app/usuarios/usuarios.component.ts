5058670a2702ca794c7d2642a7d7d6da339117c9
JHRSlzr <hernanryz@hotmail.com>
2020-10-20 02:08:52 -0500

    uppercase

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { isBuffer } from 'util';
+import { IUser } from '../app.component';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./usuarios.component.scss']
+})
+export class UsuariosComponent implements OnInit {
+
+  @Input() User : IUser;
+  @Output() outUser: EventEmitter<IUser> = new EventEmitter<IUser>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSendData() : void {
+    this.outUser.emit(this.User)
+  }
+
+  onShowButton(): boolean {
+    return Number(this.User.id) % 2 === 1;
+  }
+}
