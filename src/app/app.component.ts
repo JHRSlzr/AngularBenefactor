df1591eb060cd4ee83d3f824ecc8d42ba8b3b978
JHRSlzr <hernanryz@hotmail.com>
2020-10-20 01:55:52 -0500

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface IUser {
+  id: number;
+  name: string;
+  descripcion: string;
+  imagen: string;
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PracticaBenefactor';
+  usuarios: IUser[] = [
+    {id:1, name:'Nenuko', descripcion:'hola', imagen: '../assets/imagen/Hamster.jpg'},
+    {id:2, name:'Jorge', descripcion:'k ase', imagen: '../assets/imagen/michi.png'},
+    {id:3, name:'Hernan', descripcion:'k rollo', imagen: '../assets/imagen/nezquik.jpg'},
+    {id:4, name:'Artaban', descripcion:'k tal', imagen: '../assets/imagen/perron.jpg'},
+    {id:5, name:'Charlito', descripcion:'nose', imagen: '../assets/imagen/skubi.png'}
+  ]
+  constructor() {}
+  outUserMethod(user: IUser): void {
+    console.log((user.id)+","+ (user.name))
+  }
 }
+
+
